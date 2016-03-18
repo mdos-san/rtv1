f073124c15e30a26fd7410cda6e37607b94496f9
Mickael DOS SANTOS <mdos-san@e2r10p23.42.fr>
2016-03-19 00:50:45 +0100

    [dev]: Adding rt_v1_get_info, to have all info about the env var

--- srcs/rt_v1_init.c
+++ srcs/rt_v1_init.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 23:34:34 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/19 00:02:24 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/19 00:47:23 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ t_env	*rt_v1_init(char *file)
 	if (!(new = (t_env*)malloc(sizeof(t_env))))
 		return (NULL);
 	new->file = file;
+	rt_v1_get_info(new);
 	return (new);
 }
