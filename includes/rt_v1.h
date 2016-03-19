7b62cf15627eac8b0cef9af18911afad707eeb76
Mickael DOS SANTOS <mdos-san@e2r10p23.42.fr>
2016-03-19 02:10:25 +0100

    [dev]: Adding an obj structure (a chain list)

--- includes/rt_v1.h
+++ includes/rt_v1.h
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 07:01:17 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/19 01:11:09 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/19 02:07:50 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,29 @@
 # define WID 800
 # define HEI 600
 
-# define SPHERE 0
-# define PLAN 1
-# define CYLINDER 2
-# define CONE 3
+# define SPH 0
+# define PLA 1
+# define CYL 2
+# define CON 3
 
-typedef struct	s_env
+typedef struct		s_obj
 {
-	char	*file;
-	int		fd;
-}				t_env;
+	char			type;
+	double			x;
+	double			y;
+	double			y;
+	struct s_obj	*next;
+}					t_obj
 
-t_env			*rt_v1_init(char *file);
-void			rt_v1_exit(t_env **env, char *str);
-void			rt_v1_get_info(t_env *env);
-int				parse(t_env *env);
+typedef struct		s_env
+{
+	char			*file;
+	int				fd;
+}					t_env;
+
+t_env				*rt_v1_init(char *file);
+void				rt_v1_exit(t_env **env, char *str);
+void				rt_v1_get_info(t_env *env);
+int					parse(t_env *env);
 
 #endif
