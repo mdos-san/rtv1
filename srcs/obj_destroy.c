bad7fe9c5d0860145b1cf6c181761d87e22c812c
Mickael DOS SANTOS <mdos-san@e2r2p14.42.fr>
2016-03-24 02:22:22 +0100

    [hf]: Fixing sgev when file was bad :)

--- srcs/obj_destroy.c
+++ srcs/obj_destroy.c
@@ -6,7 +6,7 @@
 /*   By: mdos-san <mdos-san@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/20 19:30:35 by mdos-san          #+#    #+#             */
-/*   Updated: 2016/03/20 19:43:23 by mdos-san         ###   ########.fr       */
+/*   Updated: 2016/03/24 02:19:55 by mdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	obj_destroy(t_obj **obj)
 {
 	t_obj	*tmp;
 
-	tmp = (*obj)->next;
+	if (*obj)
+		tmp = (*obj)->next;
 	while (*obj)
 	{
 		if (*obj)
